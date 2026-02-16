0e064e2450bc7f4ccf86c43cdb88893064ef3192
John Ky <john@uptrust.com>
2026-02-16 14:01:34 +1000

    build(deps): add head specification for development builds
    Add head specification pointing to the main branch of the omni-dev
    repository, enabling installation of development versions directly
    from source using `brew install --head`.
    
    This allows users to test unreleased features and fixes without
    waiting for tagged releases.

--- Formula/omni-dev.rb
+++ Formula/omni-dev.rb
@@ -4,6 +4,7 @@ class OmniDev < Formula
   url "https://github.com/rust-works/omni-dev/archive/refs/tags/v0.17.0.tar.gz"
   sha256 "fc21057e4acdd8e74167d88f9ae733b49c6c5d37d6946e502bf63c02e26f2e73"
   license "BSD-3-Clause"
+  head "https://github.com/rust-works/omni-dev.git", branch: "main"
 
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
